6ad9fac5c70d51453bb6d88da245a189c1ec9791
Pratistha Parajuli <parajulipratistha@gmail.com>
2021-05-25 18:10:51 +0545

    from

--- change.js
+++ change.js
@@ -4,3 +4,16 @@ function change()
         welcome.style.color = 'white';
         alert('The color will changed to white.');
     }   
+function validate()
+{
+    var full_name = document.forms["vd"]['full_name'].value
+    if (full_name=="") 
+    {
+        alert ('Name must be entered.');
+        return false;
+    }
+    else
+    {
+        alert("Welcome to art gallery " + full_name + ". Hope you enjoy!")
+    }
+}
